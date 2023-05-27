1cf38e1438f02b0e6fde5e6de6258512538bb074
Federico Loiero <federico.loiero95@gmail.com>
2023-05-27 17:13:49 -0300

    asdad

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo Lean");
 		System.out.println("hola mundo fede");
+		System.out.println("ASDASD");
 	}
 
 }
